aed050fc8a242a8ea63221c0350b3b1f524c450f
Aruna Kumarasiri <aruna.pr@outlook.com>
2025-10-05 09:16:39 -0700

    Changed figure size

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ ax5.set_xlabel('Frequency(Hz)')
 fig.set_tight_layout(True)
 
 # plt.tight_layout() 
-fig.savefig("frequency_spectrum.png", dpi = 600)
+fig.savefig("frequency_spectrum.png", dpi = 300)
 # plt.show()
 
 
